03e378987e8437048b7003b79a8823c09476aea6
alexismouwid <alexismouwid@gmail.com>
2024-08-25 12:43:38 -0500

    mod en contenido

--- src/componentes/imagen1/index.jsx
+++ src/componentes/imagen1/index.jsx
@@ -6,17 +6,9 @@ class  Contenido extends Component {
     return (
       <> 
         <div className="imagen1-contenido">
-
+        <button className="button-compraya"> Compra ya </button> 
         </div>
-        <div className="imagen2-contenido">
-          <h1 className="titulo-imagen2"> Viaja en calma, vive en grande</h1>
-          <img src="./avianca-bird.png" alt="logo-avianca" width={200} className="logo-avianca"></img>
-          <p> Reserva y vuela entre agosto y noviembre de 2024 </p>
-
-          <p> Por trayecto desde</p> 
-          <h1 className="precio"> COP 74.600</h1> <button className="button-compra"> Compra ya</button>
-        </div>
-        
+              
       </>
  
 
