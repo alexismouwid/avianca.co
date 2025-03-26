91398e0d32e2f05e4a45a878427555bafaecbbf8
Alexis <alexismouwid@gmail.com>
2025-03-26 16:14:50 -0500

    content #1  add and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
 <Afterheader />
   <Header />
       <SearchForm /> 
+      <Contenido />
       </div>
 
 
--- src/componentes/imagen1/index.jsx
+++ src/componentes/imagen1/index.jsx
@@ -1,42 +1,31 @@
 import React, { Component } from "react";
-import './contenido.css'
-class  Contenido extends Component {
+import './contenido.css';
+import viajera from './mujer-viajera.png';
 
+class Contenido extends Component {
   render() {
     return (
       <> 
-        <content className='contenido'> 
-
-        <div className="imagen1-contenido">
-          <h1 className="espacio"> contenido de prueba</h1>
-         
-        </div>
-          <div className="imagen2-contenido">
-
-            <h1 className="titulo1"> Viaja en calma, vive en grande</h1>
-            <p> Reserva y vuela entre agosto y noviembre de 2024 </p>
-            <p> Por trayecto desde</p>
-            <h1 className="titulo2"> COP 124.300 </h1>
-            <button className="button-compraya"></button>
-
+        <div className='contenido'>
+          <div 
+            className="imagen1-contenido"
+            style={{ backgroundImage: `url(${viajera})` }}
+          >
+            {/* Contenido opcional dentro de la imagen */}
           </div>
           
-
-
-
-        </content>
-
-        
-              
+          <div className="imagen2-contenido">
+            <h1 className="titulo1">Viaja en calma, vive en grande</h1>
+            <p>Reserva y vuela entre agosto y noviembre de 2024</p>
+            <p>Por trayecto desde</p>
+            <h1 className="titulo2">COP 124.300</h1>
+            <button className="button-compraya">COMPRA YA</button>
+          </div>
+        </div>
       </>
- 
-
-
-      
-         );
+    );
   }
 }
 
 export default Contenido;
-
 
