5253cc15bd5846a0d78f02f97deb0b859ca0e1c7
alexismouwid <alexismouwid@gmail.com>
2025-04-03 11:33:55 -0500

    back suprimido del front

--- src/componentes/option-fly/index.jsx
+++ src/componentes/option-fly/index.jsx
@@ -88,7 +88,7 @@ if (!isOneWay && departureDate && returnDate && new Date(departureDate) > new Da
   };
 
   try {
-    const response = await fetch('https://backend-avianca.vercel.app/reservas', {
+    const response = await fetch('https://backend-avianca.vercel.app/reserva', {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json',
