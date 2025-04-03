c4613594547f6c3f419cf78baf7aa29558768c57
alexismouwid <alexismouwid@gmail.com>
2025-04-03 12:12:50 -0500

    new cambios

--- src/componentes/option-fly/index.jsx
+++ src/componentes/option-fly/index.jsx
@@ -88,7 +88,7 @@ if (!isOneWay && departureDate && returnDate && new Date(departureDate) > new Da
   };
 
   try {
-    const response = await fetch('https://backend-avianca.vercel.app/reservas', {
+    const response = await fetch('https://backend-avianca-co.vercel.app/reservas', {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json',
