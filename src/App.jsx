b4357c37cbe5c75a7918e7d9410a82459027733a
alexismouwid <alexismouwid@gmail.com>
2025-04-05 11:06:58 -0500

    loading integrado

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import Header from './componentes/header'
 import SearchForm from './componentes/option-fly'
 import Contenido from './componentes/imagen1'
 import Contenido2 from './componentes/content2'
+import { useState , useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  if(loading) {
+    return (
+<div className="loader-container">
+      <img src="/loader.gif" alt="Cargando..." />
+    </div>  
+    );
+  }
   return (
     <> <div className='App'>
 <div className='content-1'> 
